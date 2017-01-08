032b7089a762d8fecb4e846809a7d3f62c449260
Victor Milovanov <lost@losttech.software>
2017-01-07 21:27:57 -0800

    added LogStore project

---
+++ src/LogStore/ILogStore.cs
@@ -0,0 +1,10 @@
+﻿namespace LostTech.NKeyValue
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface ILogStore<T>
+    {
+        Task Add(DateTimeOffset timestamp, T entry);
+    }
+}
---
+++ src/LogStore/SortedLog.cs
@@ -0,0 +1,17 @@
+﻿namespace LostTech.NKeyValue
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public sealed class SortedLog<T> : ILogStore<T>
+    {
+        readonly SortedList<DateTimeOffset, T> entries = new SortedList<DateTimeOffset, T>();
+
+        public Task Add(DateTimeOffset timestamp, T entry)
+        {
+            this.entries.Add(timestamp, entry);
+            return Task.CompletedTask;
+        }
+    }
+}
