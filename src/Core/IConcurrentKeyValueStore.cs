61c2a920de98b755153556bc56749dfe09abf1ab
Victor Milovanov <lost@losttech.software>
2017-01-09 21:44:52 -0800

    added IConcurrentKeyValueStore interface

---
+++ src/Core/IConcurrentKeyValueStore.cs
@@ -0,0 +1,17 @@
+﻿namespace LostTech.NKeyValue
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IConcurrentKeyValueStore<in TKey, TTag, TValue>: IWriteableKeyValueStore<TKey, TValue>
+    {
+        Task<TaggedEntry<TTag, TValue>> GetTagged(TKey key);
+        Task<bool> Put(TKey key, TValue value, TTag tag);
+    }
+
+    public interface IConcurrentKeyValueStore<in TKey, TValue> : IWriteableKeyValueStore<TKey, TValue>
+    {
+        Task<TaggedEntry<object, TValue>> GetTagged(TKey key);
+        Task<bool> Put(TKey key, TValue value, object tag);
+    }
+}
---
+++ src/Core/TaggedEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace LostTech.NKeyValue
+{
+    public sealed class TaggedEntry<TTag, T>
+    {
+        public TTag Tag { get; set; }
+        public T Value { get; set; }
+    }
+}
