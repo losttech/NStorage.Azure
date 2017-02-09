a5b43540b5341405359dad72bd7bb4c08ca3623b
Victor Milovanov <lost@losttech.software>
2017-02-08 23:18:54 -0800

    AzureTable now implements IConcurrentKeyValueStore (not tested)

--- src/Azure/AzureTable.cs
+++ src/Azure/AzureTable.cs
@@ -3,11 +3,12 @@ namespace LostTech.NKeyValue
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
     using System.Threading.Tasks;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Table;
 
-    public sealed class AzureTable: IWriteableKeyValueStore<string, IDictionary<string, object>>
+    public sealed class AzureTable: IConcurrentKeyValueStore<string, string, IDictionary<string, object>>
     {
         readonly CloudTable table;
 
@@ -16,14 +17,32 @@ namespace LostTech.NKeyValue
             this.table = table ?? throw new ArgumentNullException(nameof(table));
         }
 
-        public async Task<IDictionary<string, object>> Get(string key) {
+        public async Task<IDictionary<string, object>> Get(string key)
+        {
+            var (found, result) = await this.TryGet(key).ConfigureAwait(false);
+            return found ? result : throw new KeyNotFoundException();
+        }
+
+        public async Task<(bool, IDictionary<string, object>)> TryGet(string key)
+        {
             if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
-            
-            var query = new TableQuery().Where(TableQuery.GenerateFilterCondition("ID", QueryComparisons.Equal, key));
+
+            var query = MakeQueryByKey(key);
             var resultSet = await this.table.ExecuteQuerySegmentedAsync(query, null).ConfigureAwait(false);
             var result = resultSet.Results.FirstOrDefault();
-            return result.Properties.ToDictionary(kv => kv.Key, kv => kv.Value.PropertyAsObject);
+            if (result == null)
+                return (false, null);
+            return (true, result.Properties.ToDictionary(kv => kv.Key, kv => kv.Value.PropertyAsObject));
+        }
+
+        private static TableQuery MakeQueryByKey(string key)
+        {
+            return new TableQuery().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition(nameof(ITableEntity.RowKey), QueryComparisons.Equal, key),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition(nameof(ITableEntity.PartitionKey), QueryComparisons.Equal, key)));
         }
 
         public Task Put(string key, IDictionary<string, object> value)
@@ -34,7 +53,7 @@ namespace LostTech.NKeyValue
                 throw new ArgumentNullException(nameof(value));
 
 
-            var entity = new AzureTableEntity(value);
+            var entity = new AzureTableEntity(key, value);
             return this.table.ExecuteAsync(TableOperation.InsertOrReplace(entity));
         }
 
@@ -50,6 +69,54 @@ namespace LostTech.NKeyValue
             var table = tableClient.GetTableReference(tableName);
             await table.CreateIfNotExistsAsync().ConfigureAwait(false);
             return new AzureTable(table);
+        }
+
+        public async Task<TaggedEntry<string, IDictionary<string, object>>> TryGetTagged(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+
+            var query = MakeQueryByKey(key);
+            var resultSet = await this.table.ExecuteQuerySegmentedAsync(query, null).ConfigureAwait(false);
+            var result = resultSet.Results.FirstOrDefault();
+            if (result == null)
+                return null;
+
+            return new TaggedEntry<string, IDictionary<string, object>>
+            {
+                Tag = result.ETag,
+                Value = result.Properties.ToDictionary(kv => kv.Key, kv => kv.Value.PropertyAsObject),
+            };
+        }
+
+        public async Task<bool> Put(string key, IDictionary<string, object> value, string tag)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            var entity = new AzureTableEntity(key, value);
+            TableOperation operation;
+            if (tag == null)
+            {
+                operation = TableOperation.Insert(entity);
+            }
+            else
+            {
+                entity.ETag = tag;
+                operation = TableOperation.InsertOrReplace(entity);
+            }
+            try
+            {
+                await this.table.ExecuteAsync(TableOperation.InsertOrReplace(entity)).ConfigureAwait(false);
+                return true;
+            }
+            catch (StorageException exception) 
+                when (exception.RequestInformation.HttpStatusCode == (int)HttpStatusCode.PreconditionFailed)
+            {
+                return false;
+            }
         }
     }
 }
--- src/Azure/AzureTableEntity.cs
+++ src/Azure/AzureTableEntity.cs
@@ -2,19 +2,23 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Table;
 
     sealed class AzureTableEntity : ITableEntity
     {
         readonly IDictionary<string, object> value;
-        public AzureTableEntity(IDictionary<string, object> value)
+        public AzureTableEntity(string rowKey, IDictionary<string, object> value, string partitionKey = null)
         {
+            this.RowKey = rowKey ?? throw new ArgumentNullException(nameof(rowKey));
+            this.PartitionKey = partitionKey ?? rowKey;
             this.value = value ?? throw new ArgumentNullException(nameof(value));
         }
-        public string ETag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string PartitionKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string RowKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string ETag { get; set; } = "*";
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
         public DateTimeOffset Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
